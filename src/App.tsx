db50256d61190e97971ad98d41d6f68258f5ccfd
chabermehl <clp.habermehl@gmail.com>
2021-04-14 15:51:20 -0600

    Create countdown timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const getTimeLeft = () => {
+    const timeOfReckoning = new Date("10/19/2021");
+    const now = new Date();
+
+    const timeLeft = timeOfReckoning.getTime() - now.getTime();
+
+    return {
+      seconds: Math.floor((timeLeft / 1000) % 60),
+      minutes: Math.floor((timeLeft / 1000 / 60) % 60),
+      hours: Math.floor((timeLeft / (1000 * 60 * 60)) % 24),
+      days: Math.floor(timeLeft / (1000 * 60 * 60 * 24)),
+    };
+  };
+
+  const [time, setTime] = useState(getTimeLeft());
+
+  useEffect(() => {
+    setInterval(() => {
+      setTime(getTimeLeft);
+    }, 1000);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: "grid", placeItems: "center" }}>
+      <h1>Time Remaining Until Zaiah's HR complaint</h1>
+      <h3>{`${time.days} Days, ${time.hours} Hours, ${time.minutes} Minutes, ${time.seconds} Seconds`}</h3>
     </div>
   );
 }
