e4a0c6b1498f65d598aee01cfb0d06dc61966796
chabermehl <clp.habermehl@gmail.com>
2021-04-14 16:33:00 -0600

    Add animated head, small cleanups

--- src/App.tsx
+++ src/App.tsx
@@ -16,17 +16,42 @@ function App() {
   };
 
   const [time, setTime] = useState(getTimeLeft());
+  const [transform, setTransform] = useState(0.05);
+  const [hovering, setHovering] = useState(false);
+
+  const handleHoverEnter = () => {
+    setHovering(true);
+  };
+
+  const handleHoverExit = () => {
+    setHovering(false);
+  };
 
   useEffect(() => {
-    setInterval(() => {
+    const interval = setInterval(() => {
       setTime(getTimeLeft);
+      setTransform((prev) => (prev === 0.05 ? -0.05 : 0.05));
     }, 1000);
+
+    return () => clearInterval(interval);
   }, []);
 
   return (
     <div style={{ display: "grid", placeItems: "center" }}>
-      <h1>Time Remaining Until Zaiah's HR complaint</h1>
+      <h1>Time Remaining Until Zaiah's HR Complaint</h1>
       <h3>{`${time.days} Days, ${time.hours} Hours, ${time.minutes} Minutes, ${time.seconds} Seconds`}</h3>
+      <div onMouseEnter={handleHoverEnter} onMouseLeave={handleHoverExit}>
+        <img
+          src="/logo512.png"
+          alt="zaiah-head"
+          style={{ transform: `rotate(${transform}turn)` }}
+        />
+        {hovering ? (
+          <h3 style={{ textAlign: "center" }}>
+            REEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE!
+          </h3>
+        ) : null}
+      </div>
     </div>
   );
 }
